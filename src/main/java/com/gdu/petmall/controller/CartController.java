7e51c213f29850d324ddf2ff061140c161823ebd
ms34k <ms34k@naver>
2023-11-22 17:38:47 +0900

    3

---
+++ src/main/java/com/gdu/petmall/controller/CartController.java
@@ -0,0 +1,65 @@
+package com.gdu.petmall.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gdu.petmall.dto.CartDto;
+import com.gdu.petmall.service.CartService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class CartController {
+
+ private final CartService cartService;
+  
+  
+  @GetMapping(value="/order/detail.do")
+  public String orderDetail(HttpServletRequest request, Model model) {
+   cartService.getList(request, model);
+   return "/order/detail";
+  }
+  
+  @GetMapping(value="/order/cart.go")
+  public String cartList(HttpServletRequest request, Model model) {
+   cartService.getList(request, model);
+   return "/order/cart";
+  }
+  
+  @ResponseBody
+  @PostMapping(value="/order/delete.do", produces="application/json")
+  public Map<String, Object> removeCart(HttpServletRequest request) throws Exception {
+    return cartService.removeCart(request);
+  }
+  
+  @ResponseBody
+  @PostMapping(value="/order/modify.do")
+  public String upDateCart(CartDto cartDto){
+    cartService.modifiyCount(cartDto);
+    return "redirect:/order/cart"; 
+  } 
+  
+  @PostMapping(value = "/order/addCart.do")
+  public String addCart(HttpServletRequest request, Model model) {
+          cartService.addCart(request, model);
+          return "redirect:/order/cart.go"; 
+  }   
+ 
+
+
+
+
+
+}
+
+  
+  
+  
