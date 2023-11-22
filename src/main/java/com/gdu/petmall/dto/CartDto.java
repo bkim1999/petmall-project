0818acb739b438190fcd3f72858ca4a55037dd04
ms34k <ms34k@naver>
2023-11-22 17:04:55 +0900

    1

---
+++ src/main/java/com/gdu/petmall/dto/CartDto.java
@@ -0,0 +1,18 @@
+package com.gdu.petmall.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class CartDto {
+
+  private int count;
+  private ProductOptionDto productOptionDto;
+  private ProductDto productDto;
+  private UserDto userDto;
+}
