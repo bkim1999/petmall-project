605bb36316a8c59427bb12361a103e0006c52e47
kangminjunes <1999koco@naver.com>
2023-11-23 16:42:17 +0900

    mj

---
+++ src/main/java/com/gdu/petmall/controller/PayController.java
@@ -0,0 +1,21 @@
+package com.gdu.petmall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gdu.petmall.service.PayService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping
+@RequiredArgsConstructor
+@Controller
+public class PayController {
+    private final PayService payService;
+
+    @GetMapping("/pay/payment.do") 
+    public String list() {
+        return "pay/payment"; 
+    }
+}
