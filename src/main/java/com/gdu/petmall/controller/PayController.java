60bfd61c2aadd96ebd4789d51754b337d59027a2
kangminjunes <1999koco@naver.com>
2023-12-04 17:25:41 +0900

    mj

--- src/main/java/com/gdu/petmall/controller/PayController.java
+++ src/main/java/com/gdu/petmall/controller/PayController.java
@@ -1,11 +1,15 @@
 package com.gdu.petmall.controller;
 
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gdu.petmall.dto.OrderDto;
 import com.gdu.petmall.service.PayService;
@@ -24,4 +28,20 @@ public class PayController {
       return "pay/payFail";
     }
     
+    //내 주문 조회로 이동
+    @GetMapping("/user/mypage/orderList.do")
+    public String getOrderList(HttpServletRequest request, Model model) {
+    	
+    	Map<String, Object> resultMap = payService.myPayList(request);
+    	model.addAttribute("myPayList", resultMap.get("myPayList"));
+    	
+      return "user/orderList";
+    }
+    
+    @GetMapping("/pay/complete")
+    public String pay(OrderDto order, RedirectAttributes redirectAttributes) {
+    	int payComplete = payService.payComplete(order);
+    	redirectAttributes.addFlashAttribute("payService", payService);
+    	return "pay/complete";
+    }
 }
--- src/main/java/com/gdu/petmall/service/PayServiceImpl.java
+++ src/main/java/com/gdu/petmall/service/PayServiceImpl.java
@@ -2,11 +2,52 @@ package com.gdu.petmall.service;
 
 import com.gdu.petmall.dao.PayMapper;
 import com.gdu.petmall.dto.OrderDto;
+import com.gdu.petmall.dto.UserDto;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Service
 public class PayServiceImpl implements PayService {
+	
+	private final PayMapper payMapper;
+    @Autowired
+    public PayServiceImpl(PayMapper payMapper) {
+        this.payMapper = payMapper;
+    }
+    @Override
+    public int getLoggedInUserNo(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("user") != null) {
+            UserDto loggedInUser = (UserDto) session.getAttribute("user");
+            return loggedInUser.getUserNo();
+        } else {
+            return -1;
+        }
+    }
+    @Override
+    public Map<String, Object> myPayList(HttpServletRequest request) {
+        int userNo = getLoggedInUserNo(request); 
 
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("userNo", userNo);
+
+        List<OrderDto> myPayList = payMapper.getMyPayList(paramMap);
+
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("myPayList", myPayList);
+
+        return resultMap;
+    }
+    @Override
+    public int payComplete(OrderDto order) {
+    	return payMapper.updatePay(order);
+    }
 }
