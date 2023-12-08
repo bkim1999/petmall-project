726ec3a1ccac6dadb9aa410c9e6e37e9c71c7103
ms34k <ms34k@naver>
2023-12-08 10:05:11 +0900

    1

--- src/main/java/com/gdu/petmall/service/FaqService.java
+++ src/main/java/com/gdu/petmall/service/FaqService.java
@@ -1,24 +1,28 @@
 package com.gdu.petmall.service;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.ui.Model;
 
+import com.gdu.petmall.dto.FaqCategoryDto;
+
 public interface FaqService {
  
-  public void getFaqList(HttpServletRequest request, Model model);
+  
+  public List<FaqCategoryDto> getloadFaqCategoryList();
+  public void customerFaqList(HttpServletRequest request, Model model);
   public void adminList(HttpServletRequest request, Model model);
   public void getSearchList(HttpServletRequest request, Model model);
-  
-  public int addFaq(HttpServletRequest request);
-  public int removeFaq(int faqNo);
-  
+  public Map<String, Object> deleteFaq(HttpServletRequest request);
   public Map<String, Object> modifyList(HttpServletRequest request);
+  public int addFaq(HttpServletRequest request);
   
-  public Map<String, Object> getloadCategoryList();
-  public Map<String, Object> getloadFaqCategoryList();
+  
+  
+  public Map<String, Object> getloadCategorySearchList(HttpServletRequest request, Model model);
   
   
 }
